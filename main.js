9e7c3f75da33988c893ec05eeab3f3d510ddbfdf
Adekoya Daniel <adekoyadaniel53@gmail.com>
2021-02-18 17:54:31 +0100

    ++

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const tl = gsap.timeline()
-tl.from('.container h1, .container p, .cta', 1, {
+tl.from('.container h1, .container p, .container button', 1, {
     transform: 'translateY(20px)',
     opacity: 0,
     stagger: .1,
@@ -27,5 +27,31 @@ ctas.forEach((cta) => {
             ease: 'expo',
             width: '70%'
         })
+        gsap.to('.info', 1, {
+            ease: 'expo',
+            opacity: 0
+        })
     })
+})
+
+const closer = document.querySelector('.closer')
+closer.addEventListener('click', () => {
+    gsap.to('.overlay', 1, {
+        opacity: 0,
+        transform: 'translateY(30px)',
+        pointerEvents: 'none',
+        ease: 'expo'
+    })
+    if(window.innerWidth >= '1570'  ){
+        gsap.to('.cards', 1, {
+            ease: 'expo',
+            width: '60%'
+        })
+    }else{
+        gsap.to('.cards', 1, {
+            ease: 'expo',
+            width: '600px'
+        })
+    }
+    
 })
